2b89d645b6bc156aa726b784bdeb8192331dde81
btramel <bradtramel@gmail.com>
2022-08-10 13:51:10 -0500

    added documentation, hover styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,24 +2,38 @@ import Link from "../node_modules/next/link"
 
 function Header() {
   return (
+    // Tailwind utility classes stylize our elements with mobile in mind first
     <header className="flex justify-between p-5 max-w-7xl mx-auto">
       <div className="flex items-center space-x-5">
+        {/* Points to homepage */}
         <Link href="/">
           <img 
             src="https://links.papareact.com/yvf"
-            className="w-44 object-contain cursor-pointer" 
+            className="w-40 hover:scale-105 object-contain cursor-pointer" 
             alt="medium logo"
             />
         </Link>
+        {/* On mobile, these elements are hidden */}
         <div className="hidden md:inline-flex items-center space-x-5">
-          <h3>About</h3>
-          <h3>Contact</h3>
+          {/* rel="noreferrer" avoids a security issue with target="_blank" in older browsers */}
+          <h3 className="hover:scale-105">
+          <a 
+          href="https://github.com/btramel/blog"
+          target="_blank"
+          rel="noreferrer"
+          >About</a></h3>
+          {/* Opens an email addressed to me */}
+          <h3 className="hover:scale-105">
+          <a
+          href="mailto:bradtramel@gmail.com"
+          >Contact</a></h3>
           <h3 className="text-white bg-green-600 px-4 py-1 rounded-full hover:bg-green-500">Follow</h3>
         </div>
       </div>
       <div className="flex items-center space-x-5 text-green-600">
-        <h3>Sign In</h3>
-        <h3 className="border px-4 py-1 rounded-full border-green-600">Get Started</h3>
+        {/* Aesthetic sign in option. Not functional */}
+        <h3 className="hover:scale-105">Sign In</h3>
+        <h3 className="sm:border sm:px-4 py-1 rounded-full border-green-600 hover:bg-green-600 hover:text-white">Get Started</h3>
       </div>
     </header>
   )
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,7 @@ export const Hero = () => {
     <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
 
       <div className="px-10 space-y-5">
+        {/* Max width applied so the display looks good on 4K and ultrawide screens */}
         <h1 className="text-6xl max-w-xl font-serif"><span className="underline decoration-black decoration-4">Medium</span> is a place to write, read, and connect</h1>
         <h2>It is easy and free to post your thinking on any topic and connect with millions of readers.</h2>
       </div>
@@ -10,7 +11,7 @@ export const Hero = () => {
       <div>
         <img 
         src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
-        className="hidden md:inline-flex h-32 lg:h-full"
+        className="hidden lg:inline-flex h-32 lg:h-full"
         alt="Medium Logo"
         />
       </div>
